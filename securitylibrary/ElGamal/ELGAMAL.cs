e01640ad38ee429fc6ab9a0fe7a63a467ffdcb8e
aymanmostafa11 <ayman20191700162@cis.asu.edu.eg>
2023-04-23 11:35:56 +0200

    Added Elgamal & Utility mod class

--- securitylibrary/ElGamal/ELGAMAL.cs
+++ securitylibrary/ElGamal/ELGAMAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SecurityLibrary.AES;
 
 namespace SecurityLibrary.ElGamal
 {
@@ -18,12 +19,25 @@ namespace SecurityLibrary.ElGamal
         /// <returns>list[0] = C1, List[1] = C2</returns>
         public List<long> Encrypt(int q, int alpha, int y, int k, int m)
         {
-            throw new NotImplementedException();
+            List<long> ans = new List<long>();
+            long c1 = Mod.Pow(alpha, k, q);
+            ans.Add(c1);
+
+            long K = Mod.Pow(y, k, q);
+            long c2 = (K * m) % q;
+            ans.Add(c2);
+
+            return ans;
 
         }
         public int Decrypt(int c1, int c2, int x, int q)
         {
-            throw new NotImplementedException();
+            int K = (int) Mod.Pow(c1, x, q);
+
+            ExtendedEuclid euclid = new ExtendedEuclid();
+            int K_1 = euclid.GetMultiplicativeInverse(K, q);
+
+            return (K_1 * c2) % q;
 
         }
     }
---
+++ securitylibrary/Utils/Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityLibrary
+{
+
+    public class Mod
+    {
+        // performs x^y % mod
+        static public long Pow(long x, long y, long mod)
+        {
+            long ans = 1;
+            for(int i = 0; i < y; i++)
+            {
+                ans = ((ans % mod) * (x % mod)) % mod;
+            }
+            return ans;
+        }
+    }
+}
