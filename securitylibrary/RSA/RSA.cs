df342110563cc10cba88caeecfd2cc300f1b41d5
said-2001 <sadevip2001@gmail.com>
2023-04-23 14:07:46 +0200

    RSA implementation

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SecurityLibrary.AES;
 
 namespace SecurityLibrary.RSA
 {
     public class RSA
     {
+        ExtendedEuclid ExtendedEuclid = new ExtendedEuclid();
         public int Encrypt(int p, int q, int M, int e)
         {
-            throw new NotImplementedException();
+            int n = p * q;
+            return (int)Mod.Pow(M, e, n);
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            int n = p * q;
+            int phi = (p - 1) * (q - 1);
+            int d = ExtendedEuclid.GetMultiplicativeInverse(e, phi);
+            return (int)Mod.Pow(C, d, n);
         }
     }
 }
