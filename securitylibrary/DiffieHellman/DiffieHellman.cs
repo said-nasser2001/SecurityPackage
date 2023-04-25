cf3678a62b6dda393bc357a07ab82ea3b3f63b70
Bassel Kordy <basselahmed95@gmail.com>
2023-04-25 23:18:59 +0200

    Added Diffie Hellman

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -10,7 +10,24 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            List<int> keys = new List<int>();
+
+            // User A Generates and sends y_a to User B from private xa
+            int ya = Mod.Pow(alpha, xa, q);
+
+            // User B Generates and sends y_b to user A from private xb
+            int yb = Mod.Pow(alpha, xb, q);
+
+            // User A Generates key from y_b and private xa
+            int key_a = Mod.Pow(yb, xa, q);
+            keys.Add(key_a);
+
+            // User B Generates key from y_a and private xb
+            int key_b = Mod.Pow(ya, xb, q);
+            keys.Add(key_b);
+
+            return keys;
+
         }
     }
 }
--- securitylibrary/Utils/Mod.cs
+++ securitylibrary/Utils/Mod.cs
@@ -19,5 +19,16 @@ namespace SecurityLibrary
             }
             return ans;
         }
+
+        static public int Pow(int x, int y, int mod)
+        {
+            int ans = 1;
+            for (int i = 0; i < y; i++)
+            {
+                ans = ((ans % mod) * (x % mod)) % mod;
+            }
+            return ans;
+        }
+
     }
 }
